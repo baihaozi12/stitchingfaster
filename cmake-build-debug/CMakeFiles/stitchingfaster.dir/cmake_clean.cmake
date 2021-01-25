c842bf3b05b8e28d0cff9d891776dd7e48695ba0
白颢 <hao.bai@traxretail.com>
2021-01-25 17:15:17 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/stitchingfaster.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stitchingfaster.dir/main.cpp.o"
+  "stitchingfaster.pdb"
+  "stitchingfaster"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stitchingfaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
